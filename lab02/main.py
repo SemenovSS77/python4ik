991b6a06e528b7e3d89173beb5b45d9e65c46247
SemenovSS77 <semenov_ss@edu.surgu.ru>
2025-02-17 23:12:14 +0500

    rare

---
+++ lab02/main.py
@@ -0,0 +1,29 @@
+def n1():
+    alphabet = ["К", "А", "Т", "Е", "Р"]
+    length = 6
+    fchar = "Р"
+    lchar = "К"
+    count = len(alphabet) ** (length - 2)
+    print(count)
+
+def n2():
+    a = 216 ** 6 + 216 ** 4 + 36 ** 5 - 6 ** 14 - 24
+    count = []
+    while a > 0:
+        count.append(a % 6)
+        a //= 6
+    count.reverse()
+    print(len(set(count)))
+
+def n3():
+    for i in range(123450708,123460000):
+        if (i % 23 == 0) and (i % 10 == 8) and ((i // 100) % 10 == 7):
+            print(i, i // 23)
+
+choice = input("")
+if choice == "n1":
+    n1()
+if choice == "n2":
+    n2()
+if choice == "n3":
+    n3()
