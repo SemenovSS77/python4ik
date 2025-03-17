2eb425e2673c7538e69ca3be1bbad1d1ef8c9d8d
SemenovSS77 <semenov_ss@edu.surgu.ru>
2025-03-17 21:24:12 +0500

    Rare

---
+++ lab05/main.py
@@ -0,0 +1,25 @@
+def apply_n_times(func, n, value):
+    for _ in range(n):
+        value = func(value)
+    return value
+
+def filter_changed(seq, func, n, threshold):
+    for item in seq:
+        new_item = apply_n_times(func, n, item)
+        if abs(new_item - item) > threshold * abs(item):
+            yield new_item
+
+def square(x):
+    return x ** 2
+
+numbers = [5, 3, 4, 1, 2]
+n = 2
+threshold = 5
+
+result_generator = filter_changed(numbers, square, n, threshold)
+
+out_filter = filter(lambda x: x > 15, result_generator)
+
+sorted_result = sorted(out_filter)
+
+print(sorted_result)
