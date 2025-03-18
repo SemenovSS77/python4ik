69f740b7c718789cc650f30ffa9523680f7268c8
SemenovSS77 <semenov_ss@edu.surgu.ru>
2025-03-18 12:45:41 +0500

    Rare

--- lab04/main.py
+++ lab04/main.py
@@ -1,15 +1,30 @@
-def check_args(f):
-    def wrapper(*args):
-        if any(not isinstance(arg, (int, float)) or not (0 <= arg <= 100) for arg in args):
-            raise ValueError("Ошибка")
-        return f(*args)
-    return wrapper
+def validate_args(ex_type, min_v, max_v):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            for arg in args:
+                if not isinstance(arg, ex_type):
+                    raise ValueError(f"Ошибка. Аргумент {arg} должен быть типа {ex_type}")
+                if not (min_v <= arg <= max_v):
+                    raise ValueError(f"Ошибка. Аргумент {arg} должен быть в диапозоне от {min_v} до {max_v}")
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
 
-@check_args
-def calculate_average(*args):
-    return sum(args) / len(args) if args else 0
+def average():
+    total = 0
+    count = 0
+    
+    @validate_args(int, 1, 100)
+    def add_number(num):
+        nonlocal total, count
+        total += num
+        count += 1
+        return total / count
+    return add_number
 
-try:
-    print(calculate_average(10, 20, 30, 100))
-except ValueError as error:
-    print(error)
+avg = average()
+
+print(avg(5.5))
+print(avg(9))
+print(avg(15))
+print(avg(100))
