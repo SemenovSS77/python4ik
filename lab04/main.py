b496b5ce7be91f00f46998f51bb5653eb32c1a71
SemenovSS77 <semenov_ss@edu.surgu.ru>
2025-05-12 18:06:04 +0500

    Medium

--- lab04/main.py
+++ lab04/main.py
@@ -1,14 +1,17 @@
-def validate_args(ex_type, min_v, max_v):
+def validate_args(ex_type=None, min_v=None, max_v=None):
     def decorator(f):
         def wrapper(*args, **kwargs):
-            for arg in args:
-                if not isinstance(arg, ex_type):
-                    raise ValueError(f"Ошибка. Аргумент {arg} должен быть типа {ex_type}")
-                if not (min_v <= arg <= max_v):
-                    raise ValueError(f"Ошибка. Аргумент {arg} должен быть в диапозоне от {min_v} до {max_v}")
+            if ex_type is not None:
+                for arg in args:
+                    if not isinstance(arg, ex_type):
+                        raise ValueError(f"Ошибка. Аргумент {arg} должен быть типа {ex_type}")
+                    if min_v is not None and max_v is not None and not (min_v <= arg <= max_v):
+                        raise ValueError(f"Ошибка. Аргумент {arg} должен быть в диапозоне от {min_v} до {max_v}")
             return f(*args, **kwargs)
+        wrapper.__wrapped__ = f
         return wrapper
     return decorator
+
 
 def average():
     total = 0
